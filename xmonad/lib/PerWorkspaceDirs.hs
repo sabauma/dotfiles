0ba3c30c321dac28b9ec07940368fb8e4ab66910
Spenser Bauman <sabauma@gmail.com>
2015-06-03 16:42:53 -0700

    HLint + Other cleanups

--- xmonad/lib/PerWorkspaceDirs.hs
+++ xmonad/lib/PerWorkspaceDirs.hs
@@ -1,30 +1,37 @@
-{-# LANGUAGE DeriveDataTypeable, NoMonomorphismRestriction, MultiParamTypeClasses, ImplicitParams #-}
+{-# LANGUAGE DeriveDataTypeable        #-}
+{-# LANGUAGE ImplicitParams            #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 -------------------------------------------------------------------------------
 -- | Allows per workspace working directories
 -- ----------------------------------------------------------------------------
 
-module PerWorkspaceDirs (
-                        currentWorkspace
-                        , changeDir
-                        , getDir
-                        ) where
+module PerWorkspaceDirs
+  ( currentWorkspace
+  , changeDir
+  , getDir
+  ) where
 
+import           Data.Maybe                  (fromMaybe)
+import           XMonad.Core
+import           XMonad.StackSet             (currentTag)
 import qualified XMonad.Util.ExtensibleState as XS
-import XMonad.Core
-import XMonad.StackSet (currentTag)
 
-data WorkingDirs = WD [(WorkspaceId, String)] deriving Typeable
+data WorkingDirs = WD { unWD :: [(WorkspaceId, String)] } deriving Typeable
 
 instance ExtensionClass WorkingDirs where
   initialValue = WD []
 
+{-changeDir :: String -> WorkspaceId -> X ()-}
+{-changeDir dir ws = do-}
+  {-WD cdirs <- XS.get-}
+  {-XS.put . WD $ (ws, dir) : filter ((/= ws) . fst) cdirs-}
+
 changeDir :: String -> WorkspaceId -> X ()
-changeDir dir ws = do (WD cdirs) <- XS.get
-                      XS.put . WD $ (ws, dir) : filter (\(a,_) -> a /= ws) cdirs
+changeDir dir ws = XS.modify $ WD . ((ws, dir) :) . filter ((/= ws) . fst) . unWD
 
 getDir :: WorkspaceId -> X String
-getDir ws = do (WD dirs) <- XS.get
-               return $ maybe "~/" id (lookup ws dirs)
+getDir ws = fmap (fromMaybe "~/" . lookup ws . unWD) XS.get
 
 currentWorkspace :: X WorkspaceId
 currentWorkspace = withWindowSet (return . currentTag)
