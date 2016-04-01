0e0620bca5941a489b3dbc9fbc2dce77630e65fc
Spenser Bauman <sabauma@gmail.com>
2016-04-01 13:43:37 -0400

    Change ups

--- xmonad/lib/PromptConfig.hs
+++ xmonad/lib/PromptConfig.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -O2 #-}
 module PromptConfig where
 
 import           Data.Char
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -O2 #-}
 
 import           XMonad
 import           XMonad.Actions.CopyWindow
