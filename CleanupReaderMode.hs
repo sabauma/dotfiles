22348da5ffcee2a92c44fb91fca32effd3840a7e
Spenser Bauman <sabauma@gmail.com>
2019-12-13 21:47:58 -0500

    Use Data.Text

--- CleanupReaderMode.hs
+++ CleanupReaderMode.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.ByteString as B
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
 import System.IO
 import Text.HTML.TagSoup.Tree
 
-removeReaderMode :: [TagTree B.ByteString] -> [TagTree B.ByteString]
+removeReaderMode :: [TagTree T.Text] -> [TagTree T.Text]
 removeReaderMode tree = transformTree f tree
   where
     f (TagBranch _ attrs _)
@@ -12,5 +13,5 @@ removeReaderMode tree = transformTree f tree
     f x = [x]
 
 main :: IO ()
-main = B.interact (renderTree . removeReaderMode . parseTree)
+main = T.interact (renderTree . removeReaderMode . parseTree)
 
